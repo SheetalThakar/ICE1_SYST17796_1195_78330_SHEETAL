344d9c293866abd3b8a7b757a0ea4480c76c221e
Kanghyun Kim <kimkan@sheridancollege.ca>
2019-05-22 12:03:34 -0400

    Finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,18 +10,57 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author sheetal
+ * @modifier 
  */
+import java.util.Random;
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+            
+            Random rand = new Random();
+            int value = rand.nextInt((13 - 1) + 1) + 1;
+            
+            c.setValue(value);
+            
+            int suit = rand.nextInt((3 - 0) + 1) + 0;
+            c.setValue(value);
+            c.setSuit(Card.SUITS[suit]);
+            System.out.println(c.getSuit() + " " + c.getValue());
+            magicHand[i] = c;
+            
+            
+            
+            
+            //print the suit and value of the card
+            //add it to MagicHand Array of Card            
+                    
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+        }
+        
+        System.out.println("Please pick a suit");
+        String suit1 = input.nextLine();        
+        System.out.println("Please pick a number");
+        int value1 = input.nextInt();
+        
+        
+        for(int i = 0; i < 1; i++){
+            if(suit1 == magicHand[i].getSuit() && value1 == magicHand[i].getValue()){
+                System.out.println("Your card matches.");
+                break;
+            }
+            else{
+                System.out.println("There are no matching cards");
+            }
+                
         }
         
         //insert code to ask the user for Card value and suit, create their card
