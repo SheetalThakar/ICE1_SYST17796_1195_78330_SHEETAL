23c24d4c24bb874b256e2f8cb31a97d2c77171d4
sehmbia <50666608+sehmbia@users.noreply.github.com>
2019-05-22 11:35:18 -0400

    Hard-coded luckyCard

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -19,7 +19,10 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+        Card luckyCard = new Card();   
+        luckyCard.setValue(1);
+        luckyCard.setSuit("Clubs");
+          
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
