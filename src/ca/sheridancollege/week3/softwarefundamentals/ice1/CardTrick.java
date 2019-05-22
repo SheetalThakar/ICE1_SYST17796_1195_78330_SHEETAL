237d951f6566ca1438ea65ca5c5e9225e3b40952
Dipalsorathiya <sorathid@sheridancollege.ca>
2019-05-22 12:01:48 -0400

    Displayed Suit and value of luckyCard - Final Commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -57,6 +57,8 @@ public class CardTrick {
         Card luckyCard=new Card();
         luckyCard.setValue(7);
         luckyCard.setSuit("Hearts");
+        
+        System.out.println(luckyCard.getValue()+" "+luckyCard.getSuit());
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
