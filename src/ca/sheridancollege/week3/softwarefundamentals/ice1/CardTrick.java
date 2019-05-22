c9345be31fc7b0c1cb877557550f10b5bda6fdb3
prachi <prachi@DESKTOP-NU2SNQT>
2019-05-22 11:34:23 -0400

    Finished adding code for 3 and 4 to CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,23 +10,56 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author sheetal
+ * @modifier Prachi
  */
+import java.util.Random;
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
-        
+         Scanner input = new Scanner(System.in);
+            int Value = input.nextInt();
+            input.nextInt();
+            String Suit = input.next();
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
+           
+            
+            Random rand = new Random();
+            int value = rand.nextInt ((13-1)+1)+ 1;
+            c.setValue(value);
             //c.setValue(insert call to random number generator here)
+             int suit = rand.nextInt((3-0)+1)+0;
+             c.setValue(value);
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setSuit(Card.SUITS[suit]);
+            System.out.println(c.getSuit() + " " +c.getValue());
+            
+            
+            magicHand[i]=c;
+        }
+            
+            
+            for(int j=0;j<magicHand.length;j++)
+            { 
+            if(magicHand[j].getValue()==Value && magicHand[j].getSuit() .equals(Suit)){
+                  System.out.println("The card is present in magic hand");
+                          }
+              
+            else{ 
+              System.out.println("The card is not present in magic hand ");
+                    }                     
+              
+            } 
+    }
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
-    }
     
-}
+    
+
