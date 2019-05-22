624d4b5ededb917027badc778876c56ad36ee08d
Kamaljit <sing8645@sheridancollege.ca>
2019-05-22 11:52:55 -0400

    Initial Commit

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,19 +10,54 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author sheetal
+ * @Kamaljit Singh
  */
+import java.util.Random;
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
     {
+        Scanner in=new Scanner(System.in);
         Card[] magicHand = new Card[7];
         
         for (int i=0; i<magicHand.length; i++)
         {
+//generate random nos. from 1-13
             Card c = new Card();
+            Random rand = new Random();
+//generate random nos. from 0-3            
+            int value=rand.nextInt((13-1)+1)+1;
+            c.setValue(value);
+            
+            int suit=rand.nextInt((3-0)+1)+0;
+            c.setSuit(Card.SUITS[suit]);
+//printing            
+            System.out.println(c.getSuit() +" " +c.getValue());
+//Add value and suit to ARRAY
+            magicHand[i]=c;
+            
             //c.setValue(insert call to random number generator here)
             //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
         }
+       
+            System.out.println("enter the value of suit ");
+            String suit1=in.nextLine();
+            System.out.println("enter the value of card");
+            int value1=in.nextInt();
+               int m=0;
+                   
+                       for (int j=0;j<7;j++){
+                           if (suit1.equals(magicHand[j].getSuit()) && (value1==magicHand[j].getValue())) {
+                               System.out.println("CArd is found");
+                               
+                               break;
+                } else System.out.println("Not found");
+                       }
+                   
+                            
+
+        
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
